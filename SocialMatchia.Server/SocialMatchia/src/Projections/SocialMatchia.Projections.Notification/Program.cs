c253c25145c88c2db867d0b3ff01eca55e1cf35d
Tuncay <tuncaycemuzun@outlook.com>
2024-03-28 22:48:22 +0300

    feat: uygulama mimarisi değiştirildi

---
+++ SocialMatchia.Server/SocialMatchia/src/Projections/SocialMatchia.Projections.Notification/Program.cs
@@ -0,0 +1,7 @@
+using SocialMatchia.Projections.Notification;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
