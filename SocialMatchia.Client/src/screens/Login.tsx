0533968011118a1e45a363617956d3b40c0f7821
Tuncay <tuncaycemuzun@outlook.com>
2024-07-05 23:41:14 +0300

    refactor: Remove unnecessary code from Login and Register screens

--- SocialMatchia.Client/src/screens/Login.tsx
+++ SocialMatchia.Client/src/screens/Login.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import { Image, KeyboardAvoidingView, Platform, StyleSheet, View } from 'react-native'
-import { globalStyles } from '../styles'
-import { Text, TextInput } from '../components'
-import Button from '../components/Button'
-import { colors } from '../utils'
+import { View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 const Login = () => {
   const navigation = useNavigation()
@@ -13,46 +8,11 @@ const Login = () => {
   const [isPasswordVisible, setIsPasswordVisible] = React.useState(true)
 
   return (
+    <View>
 
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={[globalStyles.container, {
-
-      }]}>
-      <View style={styles.form}>
-        <Text size='large'>Login</Text>
-        <Image style={styles.image} source={require('../assets/images/login.png')} />
-        <TextInput placeholder="Email"></TextInput>
-        <TextInput placeholder="Password"
-          secureTextEntry={isPasswordVisible}
-          icon={isPasswordVisible ? faEyeSlash : faEye}
-          iconPress={() => {
-            setIsPasswordVisible(!isPasswordVisible)
-          }}></TextInput>
-        <Button text="Login" size='medium' wFull></Button>
-        <Button text="Login with Google" textColor={colors.black} style={{ backgroundColor: colors.frenchGray }} size='medium' wFull></Button>
-        <Text size='small' onPress={() => navigation.navigate('Register' as never)} style={{
-          textDecorationLine: 'underline'
-        }}>Do you haven't account?</Text>
-      </View>
-
-    </KeyboardAvoidingView>
+    </View>
   )
 }
 
-const styles = StyleSheet.create({
-  image: {
-    width: '100%',
-    height: 200
-  },
-  form: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 20,
-    width: '100%',
-    height: '100%',
-  },
-
-})
 
 export default Login
--- SocialMatchia.Client/src/screens/Register.tsx
+++ SocialMatchia.Client/src/screens/Register.tsx
@@ -1,83 +1,17 @@
 import React from 'react'
-import { Image, KeyboardAvoidingView, Platform, StyleSheet, View } from 'react-native'
-import { globalStyles } from '../styles'
-import { Text, TextInput } from '../components'
-import Button from '../components/Button'
-import { colors } from '../utils'
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
-import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 
 import { useNavigation } from '@react-navigation/native'
+import { View } from 'react-native'
 
 const Register = () => {
 
-  const [isPasswordVisible, setIsPasswordVisible] = React.useState(true)
-  const [isRePasswordVisible, setIsRePasswordVisible] = React.useState(true)
 
   const navigation = useNavigation()
   return (
-
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={[globalStyles.container, {
-
-      }]}>
-      <View style={styles.form}>
-
-        <Text size='large'>Register</Text>
-        <Image style={styles.image} source={require('../assets/images/register.png')} />
-        <TextInput placeholder="Email"></TextInput>
-        <TextInput placeholder="Password"
-          secureTextEntry={isPasswordVisible}
-          icon={isPasswordVisible ? faEyeSlash : faEye}
-          iconPress={() => {
-            setIsPasswordVisible(!isPasswordVisible)
-          }}></TextInput>
-        <TextInput placeholder="Re-Password"
-          secureTextEntry={isRePasswordVisible}
-          icon={isRePasswordVisible ? faEyeSlash : faEye}
-          iconPress={() => {
-            setIsRePasswordVisible(!isRePasswordVisible)
-          }}></TextInput>
-        <Button text="Register" size='medium' wFull></Button>
-        <Button text="Register with Google" icon={faGoogle} iconPosition='left' textColor={colors.black} style={{ backgroundColor: colors.frenchGray }} size='medium' wFull></Button>
-        <Text size='small'
-          onPress={() => navigation.navigate('Login' as never)}
-          style={{
-            textDecorationLine: 'underline'
-          }}>Do you have an account?</Text>
-      </View>
-
-    </KeyboardAvoidingView>
+    <View>
+      
+    </View>
   )
 }
-
-const styles = StyleSheet.create({
-  image: {
-    width: '100%',
-    height: 200
-  },
-  form: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 20,
-    width: '100%',
-    height: '100%',
-  },
-  leftIcon: {
-    left: 0,
-    padding: 10,
-    position: 'absolute',
-    zIndex: 1,
-  },
-  rightIcon: {
-    right: 0,
-    padding: 10,
-    position: 'absolute',
-    zIndex: 1,
-    backgroundColor: 'red'
-  }
-})
-
 
 export default Register
