121f6bd7349dd3e9e437eeb939b44a30105ae362
Tuncay <tuncaycemuzun@outlook.com>
2024-07-06 17:33:19 +0300

    refactor: Update sign-in screen to use absolute imports for components

--- SocialMatchia.Client/src/App.tsx
+++ SocialMatchia.Client/src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import { PaperProvider, MD3LightTheme as DefaultTheme, } from 'react-native-paper';
 import Toast from 'react-native-toast-message';
 import Router from './router';
+import { Colors } from '@utils';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+  },
+};
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Router />
-      <Toast/>
-    </SafeAreaView>
+    <PaperProvider theme={theme}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Router />
+        <Toast />
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
--- SocialMatchia.Client/src/screens/sign-in.tsx
+++ SocialMatchia.Client/src/screens/sign-in.tsx
@@ -1,59 +1,86 @@
-import React from 'react'
-import { Image, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, View } from 'react-native'
-import { NavigationProp, ParamListBase, useNavigation } from '@react-navigation/native'
+import React from 'react';
+import { Image, KeyboardAvoidingView, Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationProp, ParamListBase, useNavigation } from '@react-navigation/native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import Toast from 'react-native-toast-message';
+import { TextInput } from 'react-native-paper';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 import { Colors } from '@utils';
 import { Button } from '@components';
-import Toast from 'react-native-toast-message';
+
+const SignInSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().min(6, 'Too Short!').required('Required'),
+});
 
 const SignIn = () => {
-  const [isPasswordVisible, setIsPasswordVisible] = React.useState(true)
-  const navigation = useNavigation<NavigationProp<ParamListBase>>()
-  
+  const [isPasswordVisible, setIsPasswordVisible] = React.useState(true);
+  const navigation = useNavigation<NavigationProp<ParamListBase>>();
+
   const handleClick = () => {
-    console.log("test")
     Toast.show({
       type: 'success',
       text1: 'Success',
       text2: 'You have successfully signed in!'
     });
-    navigation.navigate('Home')
-  }
+    navigation.navigate('Home');
+  };
 
   return (
     <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }}>
       <View style={styles.container}>
-        <Image style={styles.logo} source={require('@assets/images/logo.png')}></Image>
+        <Image style={styles.logo} source={require('@assets/images/logo.png')} />
         <Text style={styles.boldText}>Sign in to continue</Text>
-        <View style={styles.inputContainer}>
-          <TextInput placeholder="Email" style={styles.input}></TextInput>
-          <View style={{ position: 'relative' }}>
-            <TextInput secureTextEntry={isPasswordVisible} placeholder="Password" style={styles.input}></TextInput>
-            <Button style={{
-              position: 'absolute',
-              right: 10,
-              top: 15,
-              zIndex: 10
-            }} onPress={() => setIsPasswordVisible(!isPasswordVisible)}>
-              <FontAwesomeIcon icon={isPasswordVisible ? faEye : faEyeSlash} size={20} color={Colors.lightGray} />
-            </Button>
-            <Text style={{ color: 'black' }}>
-              {isPasswordVisible}
-            </Text>
-          </View>
-        </View>
-        <Button style={styles.button} onPress={() => handleClick()}>
-          <Text style={styles.buttonText}>Sign In</Text>
-        </Button>
-        <Button style={styles.signUp} onPress={() => navigation.navigate("SignUp")}>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={SignInSchema}
+          onSubmit={values => {
+            handleClick();
+          }}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+            <View style={styles.inputContainer}>
+              <TextInput
+                mode="outlined"
+                label="Email"
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                error={touched.email && !!errors.email}
+              />
+              {touched.email && errors.email && <Text style={styles.errorText}>{errors.email}</Text>}
+              <TextInput
+                mode="outlined"
+                label="Password"
+                secureTextEntry={isPasswordVisible}
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+                error={touched.password && !!errors.password}
+                right={
+                  <TextInput.Icon
+                    icon={isPasswordVisible ? "eye-off" : "eye"}
+                    onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+                  />
+                }
+              />
+              {touched.password && errors.password && <Text style={styles.errorText}>{errors.password}</Text>}
+              <Button style={styles.button} onPress={()=>handleSubmit()}>
+                <Text style={styles.buttonText}>Sign In</Text>
+              </Button>
+            </View>
+          )}
+        </Formik>
+        <TouchableOpacity style={styles.signUp} onPress={() => navigation.navigate("SignUp")}>
           <Text style={styles.signUpText}> Don't have an account? <Text style={styles.signUpLink}>Sign Up</Text></Text>
-        </Button>
+        </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -74,7 +101,7 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     width: '100%',
-    gap: 20
+    gap: 20,
   },
   input: {
     height: 50,
@@ -87,9 +114,10 @@ const styles = StyleSheet.create({
   },
   button: {
     height: 50,
-    width: '100%',
+    minWidth: '100%',
     backgroundColor: Colors.red.main,
-    borderRadius: 15,
+    borderRadius:10,
+    borderWidth: 0,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -100,16 +128,24 @@ const styles = StyleSheet.create({
   signUp: {
     left: 0,
     right: 0,
-    alignItems: 'center'
+    alignItems: 'center',
   },
   signUpText: {
-    fontSize: 14
+    fontSize: 14,
   },
   signUpLink: {
     color: Colors.red.main,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
-})
+  errorText: {
+    color: 'red',
+    fontSize: 12,
+    marginTop: -15,
+    marginBottom: 10,
+    textAlign: 'left',
+    alignSelf: 'stretch',
+  },
+});
 
-SignIn.displayName = 'SignIn'
-export default SignIn
+SignIn.displayName = 'SignIn';
+export default SignIn;
