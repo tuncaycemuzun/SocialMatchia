6bb5b750cc4251088da4e038bf5c7c305f5d06d0
Tuncay <tuncaycemuzun@outlook.com>
2024-07-06 16:20:39 +0300

    refactor: Update babel and typescript configuration for module resolution

--- SocialMatchia.Client/babel.config.js
+++ SocialMatchia.Client/babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        alias: {
+          src: './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@utils': './src/utils',
+          '@services': './src/services'
+        }
+      }
+    ]
+  ]
 };
