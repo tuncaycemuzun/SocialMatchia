2ae7187b43a6d23887d4752c043ccfd69065a1b9
Tuncay <tuncaycemuzun@outlook.com>
2024-07-06 15:02:33 +0300

    refactor: Update colors.ts to use consistent naming and structure

--- SocialMatchia.Client/src/utils/colors.ts
+++ SocialMatchia.Client/src/utils/colors.ts
@@ -1,8 +1,11 @@
-const colors = {
-  red: '#ff5252',
+const Colors = {
+  red:{
+    main: '#ff5252',
+    light : '#fdecee'
+  },
   lightGray: '#d3d3d3',
   white: '#ffffff',
   black: '#000000',
 }
 
-export default colors;
+export default Colors;
