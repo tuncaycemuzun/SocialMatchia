0d6503110ebead33313534a10b373e95dcd5cd29
Tuncay <tuncaycemuzun@outlook.com>
2024-07-06 16:20:58 +0300

    refactor: Update router.tsx to use absolute imports for screens

--- SocialMatchia.Client/src/router.tsx
+++ SocialMatchia.Client/src/router.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { SignIn, SignUp, Onboard, Home, EmailSignUp } from './screens';
+import { SignIn, SignUp, Onboard, Home, EmailSignUp } from '@screens';
 
 const Stack = createNativeStackNavigator();
 
