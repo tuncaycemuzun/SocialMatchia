08fa64e638b2f699b54d030823e5b36057423f9f
Tuncay Cem Uzun <tuncaycemuzun@outlook.com>
2024-11-23 16:10:44 +0300

    Add Profile screen and implement bottom tab navigation

--- SocialMatchia.Client/src/router.tsx
+++ SocialMatchia.Client/src/router.tsx
@@ -1,26 +1,66 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 
-import { SignIn, SignUp, Onboard, Home, EmailSignUp } from '@screens';
+import { Colors } from '@utils';
+
+import { SignIn, SignUp, Onboard, Home, EmailSignUp, Profile } from '@screens';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MainTabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let icon : any = faHome; 
+
+          if (route.name === 'Home') {
+            icon = faHome;
+          } else if (route.name === 'Profile') {
+            icon = faUser;
+          }
+          size = focused ? 28 : 24;
+          color = focused ? Colors.red.main : Colors.lightGray; 
+
+          return <FontAwesomeIcon  icon={icon} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: Colors.red.main, 
+        tabBarInactiveTintColor: '#8e8e93', 
+        tabBarStyle: {
+          height: 60,
+          paddingBottom: 5,
+        },
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+}
+
 
 export default function Router() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				screenOptions={{
-					headerShown: false,
-					animation: 'slide_from_right',
-				}}
-				initialRouteName='Home'>
-				<Stack.Screen name="SignUp" component={SignUp} />
-				<Stack.Screen name="SignIn" component={SignIn} />
-				<Stack.Screen name="Onboard" component={Onboard} />
-				<Stack.Screen name="EmailSignUp" component={EmailSignUp} />
-				<Stack.Screen name="Home" component={Home} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          animation: 'slide_from_right',
+        }}
+        initialRouteName="HomeTabs" // Adjust initial route if needed
+      >
+        <Stack.Screen name="Onboard" component={Onboard} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="EmailSignUp" component={EmailSignUp} />
+        <Stack.Screen name="HomeTabs" component={MainTabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- SocialMatchia.Client/src/screens/index.tsx
+++ SocialMatchia.Client/src/screens/index.tsx
@@ -3,3 +3,4 @@ export {default as SignIn} from './sign-in/SignIn';
 export {default as Onboard} from './onboard/Onboard';
 export {default as Home} from './home/Home';
 export {default as EmailSignUp} from './sign-up/email/EmailSignUp';
+export {default as Profile} from './profile/Profile';
